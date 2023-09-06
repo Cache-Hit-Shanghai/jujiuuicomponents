b3b9c5bf041bf6f0cd091b8b098e75828255b816
black197 <dyeh68306@163.com>
2023-09-06 14:37:19 +0800

    update

--- layout/grommet.jsx
+++ layout/grommet.jsx
@@ -1,12 +1,17 @@
-'use client'
+"use client";
 
-import { Grommet, grommet } from 'grommet';
-import StyledComponentsRegistry from './registry';
+import { Grommet, grommet } from "grommet";
+import StyledComponentsRegistry from "./registry";
 
 export default function GrommetRoot({ children }) {
   return (
     <StyledComponentsRegistry>
-      <Grommet full theme={grommet} themeMode='light' options={{ box: { cssGap: true } }}>
+      <Grommet
+        full
+        theme={grommet}
+        themeMode="light"
+        options={{ box: { cssGap: true } }}
+      >
         {children}
       </Grommet>
     </StyledComponentsRegistry>
