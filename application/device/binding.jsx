58bbb8ed450afb4bac144ce6f78740c4e93c1376
Jim Jin <jim_king_2000@163.com>
2023-09-18 11:13:58 +0800

    add DeviceBinding

--- application/device/binding.jsx
+++ application/device/binding.jsx
@@ -1,5 +1,5 @@
 import { Box, Heading, Form, FormField, TextInput } from 'grommet';
-import { Wifi } from 'grommet-icons';
+import { Wifi, Qr } from 'grommet-icons';
 import { JuJiuInformation } from '../../core/core_ui';
 
 
@@ -19,6 +19,17 @@ export function WiFiBinding() {
         </Form>
       </Box>
       <JuJiuInformation size='large' label='请输入需要网络摄像机连接的WiFi名称和密码，随后点击“下一步”。' />
+    </>
+  );
+}
+
+export function DeviceBinding() {
+  return (
+    <>
+      <Box fill='horizontal' height='medium' background='light-6' align='center' justify='center' flex={false}>
+        <Qr color='plain' size='xlarge' />
+      </Box>
+      <JuJiuInformation size='large' label='请在听到网络摄像机“绑定设备”的提示音后，将该二维码放置在摄像机镜头前约30厘米左右，等待设备“配置成功”的提示音。' />
     </>
   );
 }
