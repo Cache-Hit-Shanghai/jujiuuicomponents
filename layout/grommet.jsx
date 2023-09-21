76712bf9a365397490bba84a3960fa37feedb8f8
Jim Jin <jim_king_2000@163.com>
2023-09-21 12:23:11 +0800

    refactory

--- layout/grommet.jsx
+++ layout/grommet.jsx
@@ -4,16 +4,11 @@ import { Grommet, grommet } from 'grommet';
 import StyledComponentsRegistry from './registry';
 
 export default function GrommetRoot({ children }) {
-  return (
-    <StyledComponentsRegistry>
-      <Grommet
-        full
-        theme={grommet}
-        themeMode='light'
-        options={{ box: { cssGap: true } }}
-      >
-        {children}
-      </Grommet>
-    </StyledComponentsRegistry>
-  );
+	return (
+		<StyledComponentsRegistry>
+			<Grommet full theme={grommet} themeMode='light' options={{ box: { cssGap: true } }}>
+				{children}
+			</Grommet>
+		</StyledComponentsRegistry>
+	);
 }
