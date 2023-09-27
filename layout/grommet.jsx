1d512cd4467fe64ed63cb64921499e84533f3c88
Jim Jin <jim_king_2000@163.com>
2023-09-27 12:58:42 +0800

    refactory

--- layout/grommet.jsx
+++ layout/grommet.jsx
@@ -4,7 +4,6 @@ import { Grommet, grommet } from 'grommet';
 import StyledComponentsRegistry from './registry';
 
 export default function GrommetRoot({ themeMode, children }) {
-	console.log(themeMode);
 	return (
 		<StyledComponentsRegistry>
 			<Grommet full theme={grommet} themeMode={themeMode} options={{ box: { cssGap: true } }}>
