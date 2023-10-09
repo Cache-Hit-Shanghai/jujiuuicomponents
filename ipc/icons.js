be46147da50271e7d8f5d1891a36dd918d62027a
black197 <dyeh68306@163.com>
2023-10-09 17:59:35 +0800

    add: icons

---
+++ ipc/icons.js
@@ -0,0 +1,99 @@
+import { Mic, MicOff, Image, Settings3 } from '@styled-icons/remix-fill';
+import {
+	Volume,
+	VolumeMute,
+	Mail,
+	Expand,
+	Webcam,
+	FormNext,
+	User,
+	MoreVertical,
+	Subtract,
+	Wifi,
+	ChatOption,
+	Run,
+	Pan,
+	Alert,
+	CloudUpload,
+	Upgrade,
+	CirclePlay,
+	CircleInformation,
+	Trash,
+	ShareRounded,
+	ZoomIn,
+	RadialSelected,
+	Checkmark,
+	ClosedCaption,
+	Info,
+	Copy,
+	AddCircle,
+	Add,
+	Scan,
+	StatusGoodSmall,
+	WifiLow,
+	WifiMedium,
+	WifiNone,
+	StatusCritical,
+	Update,
+} from 'grommet-icons';
+import { FlashlightOn, FlashlightOff } from 'styled-icons/material';
+import { Record } from '@styled-icons/fluentui-system-regular/Record';
+import { RecordStop } from '@styled-icons/fluentui-system-regular/RecordStop';
+import { SettingsOutline } from '@styled-icons/evaicons-outline/SettingsOutline';
+import { CallDismiss } from '@styled-icons/fluentui-system-regular/CallDismiss';
+import { Call } from '@styled-icons/fluentui-system-regular/Call';
+import { Group } from '@styled-icons/fluentui-system-regular/Group';
+import { Share } from '@styled-icons/boxicons-regular/Share';
+import { PersonFeedback } from '@styled-icons/fluentui-system-regular/PersonFeedback';
+import { PhoneLandscape } from '@styled-icons/bootstrap/PhoneLandscape';
+
+export const JJIconMic = Mic;
+export const JJIconMicOff = MicOff;
+export const JJIconVolume = Volume;
+export const JJIconVolumeMute = VolumeMute;
+export const JJIconMail = Mail;
+export const JJIconExpand = Expand;
+export const JJIconWebcam = Webcam;
+export const JJIconFormNext = FormNext;
+export const JJIconUser = User;
+export const JJIconMoreVertical = MoreVertical;
+export const JJIconSubtract = Subtract;
+export const JJIconWifi = Wifi;
+export const JJIconWifiLow = WifiLow;
+export const JJIconWifiMediem = WifiMedium;
+export const JJIconWifiNone = WifiNone;
+export const JJIconChatOption = ChatOption;
+export const JJIconRun = Run;
+export const JJIconPan = Pan;
+export const JJIconAlert = Alert;
+export const JJIconSettings = Settings3;
+export const JJIconFlashlightOn = FlashlightOn;
+export const JJIconFlashlightOff = FlashlightOff;
+export const JJIconImage = Image;
+export const JJIconCloudUpload = CloudUpload;
+export const JJIconUpgrade = Upgrade;
+export const JJIconCirclePlay = CirclePlay;
+export const JJIconCircleInformation = CircleInformation;
+export const JJIconTrash = Trash;
+export const JJIconShareRounded = ShareRounded;
+export const JJIconRecord = Record;
+export const JJIconRecordStop = RecordStop;
+export const JJIconZoomIn = ZoomIn;
+export const JJIconRadialSelected = RadialSelected;
+export const JJIconCheckMark = Checkmark;
+export const JJIconClosedCaption = ClosedCaption;
+export const JJIconSettingsOutline = SettingsOutline;
+export const JJIconInfo = Info;
+export const JJIconCallDismiss = CallDismiss;
+export const JJIconCall = Call;
+export const JJIconCopy = Copy;
+export const JJIconAddCircle = AddCircle;
+export const JJIconAdd = Add;
+export const JJIconGroup = Group;
+export const JJIconScan = Scan;
+export const JJIconShare = Share;
+export const JJIconPersonFeedback = PersonFeedback;
+export const JJIconStatusGoodSmall = StatusGoodSmall;
+export const JJIconPhoneLandscape = PhoneLandscape;
+export const JJIconStatusCritical = StatusCritical;
+export const JJIconUpdate = Update;
