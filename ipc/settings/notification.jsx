1f32f48876cd48367681274ff7590110e1ee7c48
jujiu <jujiu@outlook.com>
2023-10-11 14:27:22 +0800

    noti set component

--- ipc/settings/notification.jsx
+++ ipc/settings/notification.jsx
@@ -4,9 +4,8 @@ import { CheckBox } from 'grommet';
 import { JuJiuItem } from '../../core/core-item';
 import { useJuJiuT } from '@/state/translate';
 import { useState, useCallback } from 'react';
-import { NotificationApi } from 'jujiu-ipc-common/api/net/notification';
 
-const keyList = [
+const notiKeyList = [
 	{ title: '设备访问', key: 'deviceAccess' },
 	{ title: '设备状态', key: 'deviceState' },
 	{ title: '设备异常', key: 'deviceDeviant' },
@@ -15,58 +14,24 @@ const keyList = [
 	{ title: '公告', key: 'notice' },
 ];
 
-const typekeyList = [
-	{ key: 'framePeopleMove', label: '画面有人移动' },
-	{ key: 'frameFaceDetection', label: '画面人脸识别结果' },
-	{ key: 'nursePeopleMove', label: '看护区域有人移动' },
+const notiTypeKeyList = [
+	{ key: 'framePeopleMove', title: '画面有人移动' },
+	{ key: 'frameFaceDetection', title: '画面人脸识别结果' },
+	{ key: 'nursePeopleMove', title: '看护区域有人移动' },
 ];
 
-export function ChangeNotification({ data }) {
-	const [notiSet, setNotiSet] = useState(() => {
-		const d = {};
-		keyList.map(({ key } = {}) => {
-			d[key] = data[key];
-		});
-		return d;
-	});
+export function ChangeNotification({ list = notiKeyList, data = {}, onChange = () => {} }) {
 	const t = useJuJiuT();
 
-	const onChange = useCallback(
-		({ target: { checked, dataset } }) => {
-			notiSet[dataset.key] = checked; //! state is immutable
-			setNotiSet({ ...notiSet });
-			NotificationApi.updateDebounce(notiSet);
-		},
-		[notiSet]
-	);
-
-	return keyList.map(({ title, key } = {}) => (
+	return list.map(({ title, key } = {}) => (
 		<JuJiuItem key={key} label={t(title)}>
-			<CheckBox toggle data-key={key} checked={notiSet[key]} onChange={onChange} />
+			<CheckBox toggle data-key={key} checked={data[key]} onChange={onChange} />
 		</JuJiuItem>
 	));
 }
 
-export function ChangeAIAlarm({ data = {} }) {
-	const [event, setEvent] = useState(data.aiAlarmEvent || {});
-	const t = useJuJiuT();
-
-	const onChange = useCallback(
-		({ target: { checked, dataset } }) => {
-			//console.log('data-key', target.dataset, target.checked);
-			event[dataset.key] = checked;
-			setEvent({ ...event });
-
-			NotificationApi.updateDebounce({
-				aiAlarmEvent: event,
-			});
-		},
-		[event]
-	);
-
-	return typekeyList.map(({ key, label }) => (
-		<JuJiuItem label={t(label)} key={key}>
-			<CheckBox checked={event[key]} data-key={key} toggle onChange={onChange} />
-		</JuJiuItem>
-	));
+export function ChangeAIAlarm({ list = notiTypeKeyList, ...props }) {
+	return <ChangeNotification list={list} {...props}></ChangeNotification>;
 }
+
+export { notiKeyList, notiTypeKeyList };
