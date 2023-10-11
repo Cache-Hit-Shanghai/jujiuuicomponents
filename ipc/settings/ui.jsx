33daad617eb2af4dee960b234370e08e2c5bbcfb
jujiu <jujiu@outlook.com>
2023-10-11 15:04:28 +0800

    set ui

--- ipc/settings/ui.jsx
+++ ipc/settings/ui.jsx
@@ -2,23 +2,65 @@
 
 import { Select, Box } from 'grommet';
 import { JuJiuItem } from '../../core/core-item';
+import { useJuJiuT } from '@/state/translate';
 
-export function ChangeUI() {
+export function ChangeUI({ langProps = {}, fontProps = {}, themeProps = {} }) {
+	const t = useJuJiuT();
+
 	return (
 		<>
-			<JuJiuItem label='语言'>
+			<JuJiuItem label={t('语言')}>
 				<Box width='small'>
-					<Select options={['简体中文', 'English']} />
+					<Select
+						name='language'
+						labelKey={'label'}
+						valueKey={{
+							key: 'value',
+							reduce: true,
+						}}
+						options={[
+							{ label: '简体中文', value: 'zh' },
+							{ label: 'English', value: 'en' },
+						]}
+						{...langProps}
+					/>
 				</Box>
 			</JuJiuItem>
-			<JuJiuItem label='字体大小'>
+			<JuJiuItem label={t('字体大小')}>
 				<Box width='small'>
-					<Select options={['大字体', '小字体']} />
+					<Select
+						name='fontsize'
+						value={'normal'}
+						labelKey={'label'}
+						valueKey={{
+							key: 'value',
+							reduce: true,
+						}}
+						options={[
+							{ label: t('小字体'), value: 'small' },
+							{ label: t('大字体'), value: 'large' },
+						]}
+						disabled={true}
+						{...fontProps}
+					/>
 				</Box>
 			</JuJiuItem>
-			<JuJiuItem label='主题'>
+			<JuJiuItem label={t('主题')}>
 				<Box width='small'>
-					<Select options={['浅色模式', '深色模式', '跟随系统']} />
+					<Select
+						name='theme-mode'
+						labelKey={'label'}
+						valueKey={{
+							key: 'value',
+							reduce: true,
+						}}
+						options={[
+							{ label: t('浅色模式'), value: 'light' },
+							{ label: t('深色模式'), value: 'dark' },
+							{ label: t('跟随系统'), value: 'auto' },
+						]}
+						{...themeProps}
+					/>
 				</Box>
 			</JuJiuItem>
 		</>
