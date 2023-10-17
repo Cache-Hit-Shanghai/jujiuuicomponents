733f32f553518847500105f72fea2aaa2b667898
black197 <dyeh68306@163.com>
2023-10-17 11:21:15 +0800

    add: ota

---
+++ ipc/ota/info.js
@@ -0,0 +1,44 @@
+'use client';
+
+import { useJuJiuT } from '@/state/translate';
+import { Box, Text, Heading } from 'grommet';
+import { JJIconUpdate } from '../icons';
+import { ButtonLink } from '../../core';
+
+function OtaUpgradeInfo({ device, info, basePath = '' }) {
+	const { currentVersion, upgradeVersion, updateLog = '' } = info;
+	const { desc, _id } = device;
+	const updateLogInLines = updateLog.split(/[\n\r]+/).filter((v) => v);
+	const t = useJuJiuT();
+
+	return (
+		<>
+			<Box margin='large' align='center'>
+				<JJIconUpdate size='large' />
+			</Box>
+			<Box align='center'>
+				<Text>
+					{t('最新版本')}：{upgradeVersion}
+				</Text>
+				<Text>
+					{t('当前版本')}：{currentVersion}
+				</Text>
+			</Box>
+			<Box>
+				<Heading level={2}>{t('更新日志')}：</Heading>
+				{updateLogInLines.map((line, i) => (
+					<Text key={i}>{line}</Text>
+				))}
+			</Box>
+			<Box direction='row' justify='center'>
+				<ButtonLink
+					href={{ pathname: basePath + '/ota/upgrade', query: { _id } }}
+					primary
+					label={t('现在升级')}
+				/>
+			</Box>
+		</>
+	);
+}
+
+export { OtaUpgradeInfo };
---
+++ ipc/ota/upgrade.js
@@ -0,0 +1,41 @@
+'use client'
+
+import { useJuJiuT } from '@/state/translate';
+import { Heading, Box, Avatar, Text, Meter } from 'grommet';
+import { JJIconWebcam } from './../icons';
+import { JuJiuInformation } from '../../core';
+
+function OtaUpgradeView({ device, progress, maxProgress, children }) {
+	const { desc, usn } = device;
+	const isUpgradeOver = progress === maxProgress;
+
+	const t = useJuJiuT();
+
+	return (
+		<>
+			{!isUpgradeOver && (
+				<Heading margin='none' level={2}>
+					{t('固件升级中……')}
+				</Heading>
+			)}
+			<Box align='center'>
+				<Avatar background='background-front'>
+					<JJIconWebcam />
+				</Avatar>
+				<Text>{desc}</Text>
+			</Box>
+			<JuJiuInformation
+				label={t('摄像机正在固件升级中。请保持摄像机的电源开启。摄像机突然断电有可能导致设备无法启动。')}
+			/>
+			<Meter round values={[{ value: progress }]} max={maxProgress} />
+			{isUpgradeOver && (
+				<Box align='center' gap='medium'>
+					<Text>{t('固件升级成功！设备正在重启中，请耐心等待。设备重启后即可正常使用。')}</Text>
+					<Box direction='row'>{children}</Box>
+				</Box>
+			)}
+		</>
+	);
+}
+
+export { OtaUpgradeView };
