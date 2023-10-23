2f8212c6f6cc89f7a7f8cf91b30f5f97774f5596
black197 <dyeh68306@163.com>
2023-10-23 16:44:53 +0800

    update: IpcCardRawRobot, video

--- ipc/device/avatar.jsx
+++ ipc/device/avatar.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Card, CardBody, CardFooter, Image, Stack } from 'grommet';
+import { Box, Card, CardBody, CardFooter, Image, Stack, Video } from 'grommet';
 import { BatteryCharge } from '@styled-icons/typicons/BatteryCharge';
 import {
 	JuJiuTagDeviceOnline,
@@ -25,6 +25,7 @@ function LinkOrNone({ url, children }) {
 export function IpcCardRawRobot({
 	label,
 	imgurl,
+	videoUrl,
 	nextPageUrl,
 	online = false,
 	cloudStorage = 'expiring',
@@ -35,10 +36,25 @@ export function IpcCardRawRobot({
 			<CardBody background='background-front'>
 				<LinkOrNone url={nextPageUrl}>
 					<Stack as='a'>
-						<Box height={'200px'}>{imgurl && <Image src={imgurl} fit='cover' alt='' />}</Box>
+						<Box height={'200px'}>
+							{imgurl && <Image src={imgurl} fit='cover' alt='' />}
+							{videoUrl && (
+								<Video
+									src={videoUrl}
+									fit='cover'
+									autoPlay={true}
+									controls={false}
+									loop={true}
+									mute={true}
+									style={{ zIndex: 0 }}
+								/>
+							)}
+						</Box>
 						<Box direction='row' margin='medium' align='center' justify='between'>
 							<Box direction='row' gap='small'>
-								<Box direction='row'>{online ? <JuJiuTagDeviceOnline /> : <JuJiuTagDeviceOffline />}</Box>
+								<Box direction='row'>
+									{online ? <JuJiuTagDeviceOnline /> : <JuJiuTagDeviceOffline />}
+								</Box>
 								<Box direction='row'>
 									{
 										{
@@ -79,14 +95,17 @@ export function IpcCardRaw({
 						<Box height={'200px'}>{imgurl && <Image src={imgurl} fit='cover' alt='' />}</Box>
 						<Box direction='row' margin='medium' gap='small'>
 							<Box gap='small'>
-								<Box direction='row'>{online ? <JuJiuTagDeviceOnline /> : <JuJiuTagDeviceOffline />}</Box>
+								<Box direction='row'>
+									{online ? <JuJiuTagDeviceOnline /> : <JuJiuTagDeviceOffline />}
+								</Box>
 							</Box>
 							<Box gap='small'>
 								<Box direction='row'>
 									{
-										{ expiring: <JuJiuTagCloudStorageExpiring />, expired: <JuJiuTagCloudStorageExpired /> }[
-											cloudStorage
-										]
+										{
+											expiring: <JuJiuTagCloudStorageExpiring />,
+											expired: <JuJiuTagCloudStorageExpired />,
+										}[cloudStorage]
 									}
 								</Box>
 							</Box>
