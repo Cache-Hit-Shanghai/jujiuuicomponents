c37080d2c7cc2973c01affa695511917580487e9
Jim Jin <jim_king_2000@163.com>
2023-10-30 13:33:02 +0800

    add font-family

--- layout/grommet.jsx
+++ layout/grommet.jsx
@@ -1,12 +1,21 @@
 'use client';
 
 import { Grommet, grommet } from 'grommet';
+import { deepMerge } from 'grommet/utils';
 import { StyledComponentsRegistry } from './registry';
+
+const myTheme = deepMerge(grommet, {
+	global: {
+		font: {
+			family: `-apple-system, BlinkMacSystemFont, "PingFang SC","Helvetica Neue",STHeiti,"Microsoft Yahei",Tahoma,Simsun,sans-serif`,
+		},
+	},
+});
 
 function GrommetRoot({ themeMode, children }) {
 	return (
 		<StyledComponentsRegistry>
-			<Grommet full theme={grommet} themeMode={themeMode} options={{ box: { cssGap: true } }}>
+			<Grommet full theme={myTheme} themeMode={themeMode} options={{ box: { cssGap: true } }}>
 				{children}
 			</Grommet>
 		</StyledComponentsRegistry>
