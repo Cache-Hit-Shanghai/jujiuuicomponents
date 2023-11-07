6fc35040d38238d25716d87148f68fb5d59ebb83
Jim <jim_king_2000@163.com>
2023-11-07 23:20:33 +0800

    add LinkGroup

--- nextui/core/core-ui.js
+++ nextui/core/core-ui.js
@@ -1,8 +1,8 @@
 'use client';
 
-import { Button } from '@nextui-org/react';
+import { Button, Card, CardBody, Listbox, ListboxItem  } from '@nextui-org/react';
 import { useRouter } from '@/state/translate';
-import { ArrowUturnLeftIcon } from '@heroicons/react/24/solid';
+import { ArrowUturnLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 
 export function ButtonBack({ onClick }) {
 	const router = useRouter();
@@ -18,5 +18,21 @@ export function ButtonBack({ onClick }) {
 		>
 			<ArrowUturnLeftIcon className='h-6 w-6' />
 		</Button>
+	);
+}
+
+export function LinkGroup({ data }) {
+	return (
+		<Card>
+			<CardBody>
+				<Listbox aria-label='LinkGroup'>
+          {data.map(datum => (
+            <ListboxItem key={datum.label} textValue={datum.label} href={datum.url} endContent={<ChevronRightIcon className='h-6 w-6' />}>
+              {datum.label}
+            </ListboxItem>
+          ))}
+        </Listbox>
+			</CardBody>
+		</Card>
 	);
 }
--- nextui/ipc/about/brand.jsx
+++ nextui/ipc/about/brand.jsx
@@ -9,21 +9,24 @@ export function AppLogo() {
 
 	return (
 		<Card>
-			<CardBody className='px-0 py-4'>
+			<CardBody className='px-0 py-6 relative'>
 				<div className='flex gap-2 items-center justify-center'>
 					<Pets size='48' />
-					<p className='text-large'>{t('小皮部落')}</p>
+					<p className='text-4xl font-extrabold'>{t('小皮部落')}</p>
 				</div>
 				<Spacer y={4} />
 				<div className='flex flex-col items-center'>
 					<p>{t('slogan')}</p>
+					<Spacer y={1} />
 					<p className='text-small'>
 						Powered by {t('光方云')}
 						<sup>&reg; </sup>
+					</p>
+					<div className='absolute top-1 right-1'>
 						<Chip size='sm' color='warning'>
 							IPv6
 						</Chip>
-					</p>
+					</div>
 				</div>
 			</CardBody>
 		</Card>
@@ -35,11 +38,14 @@ export function AppMark() {
 
 	return (
 		<div className='p-2'>
-			<p className='text-[96px] text-center'>
+			<p className='text-2xl text-center'>
 				{t('小皮部落')}
 				<sup>&reg;</sup>
 			</p>
-			<p className='text-tiny text-default-400'>&copy;2021-2023 {t('上海光方迅视科技有限公司版权所有')}</p>
+			<Spacer y={1} />
+			<p className='text-tiny text-center text-default-400'>
+				&copy;2021-2023 {t('上海光方迅视科技有限公司版权所有')}
+			</p>
 		</div>
 	);
 }
