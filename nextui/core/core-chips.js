2c5219d09deef7a475a7ea9810a1f93ea1a7185d
Jim Jin <jim_king_2000@163.com>
2023-11-09 18:27:39 +0800

    add new components

--- nextui/core/core-chips.js
+++ nextui/core/core-chips.js
@@ -4,10 +4,10 @@ import { Button, Card, CardBody, Listbox, ListboxItem, Tabs, Chip } from '@nextu
 import { useTheme } from 'next-themes';
 import Link, { useJuJiuT, useRouter, usePathname } from '@/state/translate';
 import {
-	SunIcon,
-	MoonIcon,
-	UserCircleIcon,
-	HomeModernIcon,
+	ArrowTopRightOnSquareIcon,
+	CloudIcon,
+	ArrowLeftOnRectangleIcon,
+	Battery50Icon,
 	ArrowUturnLeftIcon,
 	ChevronRightIcon,
 	MagnifyingGlassIcon,
@@ -27,4 +27,44 @@ export function ChipOffline() {
 	const t = useJuJiuT();
 
 	return <Chip variant='dot'>{t('离线')}</Chip>;
+}
+
+export function ChipFromFriends() {
+	const t = useJuJiuT();
+
+	return (
+		<Chip color='primary' startContent={<ArrowLeftOnRectangleIcon className='h-5 w-5' />}>
+			{t('来自分享')}
+		</Chip>
+	);
+}
+
+export function ChipShared() {
+	const t = useJuJiuT();
+
+	return (
+		<Chip color='secondary' startContent={<ArrowTopRightOnSquareIcon className='h-5 w-5' />}>
+			{t('分享中')}
+		</Chip>
+	);
+}
+
+export function ChipCloudStorageExpiring() {
+	const t = useJuJiuT();
+
+	return (
+		<Chip color='warning' startContent={<CloudIcon className='h-5 w-5' />}>
+			{t('云存储即将过期')}
+		</Chip>
+	);
+}
+
+export function ChipCloudStorageExpired() {
+	const t = useJuJiuT();
+
+	return (
+		<Chip color='danger' startContent={<CloudIcon className='h-5 w-5' />}>
+			{t('云存储已过期')}
+		</Chip>
+	);
 }
