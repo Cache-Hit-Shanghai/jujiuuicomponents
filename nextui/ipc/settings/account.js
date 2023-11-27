ecbe9c9c7bfff387e28fdd53fcebfdbec640a627
Jim Jin <jim_king_2000@163.com>
2023-11-27 18:38:09 +0800

    refactory

--- nextui/ipc/settings/account.js
+++ nextui/ipc/settings/account.js
@@ -8,10 +8,10 @@ export function ChangePassword({ onSubmit = () => {} }) {
 	return (
 		<Form onSubmit={onSubmit}>
 			<div className='flex flex-col gap-4'>
-				<Input label={t('输入旧密码') + ':'} type='password' name='oldPwd' />
-				<Input label={t('输入新密码') + ':'} type='password' name='newPwd' />
-				<Input label={t('再次输入新密码') + ':'} type='password' name='validateNewPwd' />
-				<Button type='submit' color='primary'>
+				<Input size='sm' label={t('输入旧密码') + ':'} type='password' name='oldPwd' />
+				<Input size='sm' label={t('输入新密码') + ':'} type='password' name='newPwd' />
+				<Input size='sm' label={t('再次输入新密码') + ':'} type='password' name='validateNewPwd' />
+				<Button size='sm' type='submit' color='primary'>
 					{t('确定')}
 				</Button>
 			</div>
