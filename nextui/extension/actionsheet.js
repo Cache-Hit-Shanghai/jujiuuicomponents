9783a1399f84ac1f064dbd6486f4222a1c1e3691
Jim Jin <jim_king_2000@163.com>
2023-11-29 15:15:01 +0800

    refactory

--- nextui/extension/actionsheet.js
+++ nextui/extension/actionsheet.js
@@ -1,21 +1,39 @@
-import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from '@nextui-org/react';
+import { Modal, ModalContent, Link, Divider, Card, CardHeader, CardBody } from '@nextui-org/react';
+import { useJuJiuT } from '@/state/translate';
 
-export function ActionSheet({ title, buttons }) {
+export function ActionSheet({ isOpen, onOpenChange, title, buttons }) {
+	const t = useJuJiuT();
+
 	return (
-		<Modal backdrop='blur' placement='center' isOpen={isOpen} onClose={onClose}>
-			<ModalContent>
+		<Modal
+			backdrop='blur'
+			placement='bottom'
+			hideCloseButton={true}
+			isOpen={isOpen}
+			onOpenChange={onOpenChange}
+			className='bg-transparent'
+		>
+			<ModalContent className='gap-2'>
 				{(onClose) => (
 					<>
-						<ModalHeader className='flex flex-col gap-1'>{title}</ModalHeader>
-						<ModalBody>{message}</ModalBody>
-						<ModalFooter>
-							<Button color='danger' variant='light' onPress={onClose}>
-								{t('否')}
-							</Button>
-							<Button color='primary' onPress={onClose}>
-								{t('是')}
-							</Button>
-						</ModalFooter>
+						<Card>
+							<CardHeader className='justify-center text-default-500'>{title}</CardHeader>
+							<Divider />
+							<CardBody className='gap-2'>
+								{buttons.map(({ label, key, ...props }) => (
+									<Link key={key} className='py-1 justify-center' {...props}>
+										{label}
+									</Link>
+								))}
+							</CardBody>
+						</Card>
+						<Card>
+							<CardBody>
+								<Link onPress={onClose} className='py-1 justify-center'>
+									{t('取消')}
+								</Link>
+							</CardBody>
+						</Card>
 					</>
 				)}
 			</ModalContent>
