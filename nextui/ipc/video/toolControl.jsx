bdf4e2ed2d3bf11cd56181768ce2bf66c55acb66
Jim Jin <jim_king_2000@163.com>
2023-12-06 17:23:57 +0800

    modify resolution button

--- nextui/ipc/video/toolControl.jsx
+++ nextui/ipc/video/toolControl.jsx
@@ -1,6 +1,21 @@
 'use client';
 
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from '@nextui-org/react';
+import {
+	Dropdown,
+	DropdownTrigger,
+	DropdownMenu,
+	Divider,
+	Button,
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	useDisclosure,
+	Listbox,
+	ListboxSection,
+	ListboxItem,
+} from '@nextui-org/react';
 import { useState } from 'react';
 import { PhotoCamera } from '@styled-icons/material/PhotoCamera';
 import { Phone } from '@styled-icons/material/Phone';
@@ -17,6 +32,7 @@ import { Battery3Bar } from '@styled-icons/material/Battery3Bar';
 import { Download } from '@styled-icons/material/Download';
 import { useJuJiuT } from '@/state/translate';
 import { LinkButton } from '../../core/core-ui';
+import { ActionSheet } from '../../extension/actionsheet';
 
 /**
  * @typedef {import("@nextui-org/react").ButtonProps} ButtonProps
@@ -105,28 +121,52 @@ export function RecordControl({ showLabel, recording, ...prop }) {
 export function ResolutionControl({ showLabel, items, ...prop }) {
 	const t = useJuJiuT();
 	const label = t('清晰度');
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+	const [selectedKeys, setSelectedKeys] = useState(new Set(['2.5k']));
 
 	return (
-		<Dropdown>
-			<DropdownTrigger>
-				<Button className='p-0 min-w-fit' isIconOnly={!showLabel} variant='light' radius='none' {...prop}>
-					<div className='flex flex-col items-center'>
-						<Hd size={24} />
-						{showLabel && <p className='text-xs'>{label}</p>}
-					</div>
-				</Button>
-			</DropdownTrigger>
-			<DropdownMenu
-				aria-label='resolution'
-				disallowEmptySelection
-				selectionMode='single'
-				selectedKeys={['1080p']}
+		<>
+			<Button
+				className='p-0 min-w-fit'
+				isIconOnly={!showLabel}
+				onPress={onOpen}
+				variant='light'
+				radius='none'
+				{...prop}
 			>
-				<DropdownItem key='2.5k'>{t('超清')}</DropdownItem>
-				<DropdownItem key='1080p'>{t('高清')}</DropdownItem>
-				<DropdownItem key='720p'>{t('标清')}</DropdownItem>
-			</DropdownMenu>
-		</Dropdown>
+				<div className='flex flex-col items-center'>
+					<Hd size={24} />
+					{showLabel && <p className='text-xs'>{label}</p>}
+				</div>
+			</Button>
+			<Modal hideCloseButton isOpen={isOpen} onOpenChange={onOpenChange}>
+				<ModalContent>
+					{(onClose) => (
+						<>
+							<ModalHeader className='flex flex-col items-center'>{label}</ModalHeader>
+							<Divider />
+							<ModalBody>
+								<Listbox
+									aria-label='resolution'
+									variant='flat'
+									disallowEmptySelection
+									selectionMode='single'
+									selectedKeys={selectedKeys}
+									onSelectionChange={(e) => {
+										setSelectedKeys(e);
+										onClose();
+									}}
+								>
+									<ListboxItem key='2.5k'>{t('超清')}</ListboxItem>
+									<ListboxItem key='1080p'>{t('高清')}</ListboxItem>
+									<ListboxItem key='720p'>{t('标清')}</ListboxItem>
+								</Listbox>
+							</ModalBody>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+		</>
 	);
 }
 
