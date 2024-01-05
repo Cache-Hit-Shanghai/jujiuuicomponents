5dd02e21e59e232b045fa80466c5e844ebb2875b
fbhou <qxju2020@sjtu.edu.cn>
2024-01-05 18:00:11 +0800

    migrate toast2 to toast

--- nextui/extension/toast.js
+++ nextui/extension/toast.js
@@ -1,42 +1,67 @@
-import { Button } from '@nextui-org/react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { Button, Card, CardBody } from '@nextui-org/react';
 import { Close } from '@styled-icons/material/Close';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect } from 'react';
-import { twMerge } from 'tailwind-merge';
+import { useRef } from 'react';
+import { useToast } from '@react-aria/toast';
+import { useToastRegion } from '@react-aria/toast';
+import { useToastState } from '@react-stately/toast';
 
-const PositionMap = new Map([
-	['top', 'top-0'],
-	['center', 'inset-y-0 h-fit mx-2 my-auto'],
-	['bottom', 'bottom-0'],
-]);
-
-export function Toast({ isOpen, onClose, message, startContent, position = 'bottom', duration = 5000 }) {
-	useEffect(() => {
-		if (isOpen && duration) {
-			const tid = setTimeout(onClose, duration);
-			return () => clearTimeout(tid);
-		}
-	}, [isOpen, duration]);
+function Toast({ state, ...props }) {
+	const ref = useRef(null);
+	const { toastProps, titleProps, closeButtonProps } = useToast(props, state, ref);
 
 	return (
-		<AnimatePresence>
-			{isOpen && (
-				<motion.div
-					initial={{ y: '150%' }}
-					animate={{ y: 0 }}
-					exit={{ y: '150%' }}
-					className={twMerge(
-						'fixed z-[1000] rounded-medium inset-x-0 bg-default p-3 m-2 text-sm flex flex-row items-center gap-2',
-						PositionMap.get(position),
-					)}
-				>
-					{startContent}
-					<div className='flex-1'>{message}</div>
-					<Button isIconOnly variant='light' onPress={onClose}>
+		<motion.div
+			{...toastProps}
+			ref={ref}
+			layout
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+		>
+			<Card>
+				<CardBody className='flex flex-row items-center justify-between'>
+					<div className='text-sm' {...titleProps}>
+						{props.toast.content}
+					</div>
+					<Button isIconOnly variant='light' radius='none' {...closeButtonProps}>
 						<Close size={24} />
 					</Button>
-				</motion.div>
-			)}
-		</AnimatePresence>
+				</CardBody>
+			</Card>
+		</motion.div>
+	);
+}
+
+function ToastRegion({ state, ...props }) {
+	const ref = useRef(null);
+	const { regionProps } = useToastRegion(props, state, ref);
+
+	return (
+		<motion.div
+			{...regionProps}
+			ref={ref}
+			className='fixed flex flex-col bottom-0 inset-x-0 p-2 gap-2 z-50 outline-none'
+			layout
+		>
+			<AnimatePresence>
+				{state.visibleToasts.map((toast) => (
+					<Toast key={toast.key} toast={toast} state={state} />
+				))}
+			</AnimatePresence>
+		</motion.div>
+	);
+}
+
+export function ToastProvider({ children, ...props }) {
+	const state = useToastState({
+		maxVisibleToasts: 5,
+	});
+
+	return (
+		<>
+			{children(state)}
+			{state.visibleToasts.length > 0 && <ToastRegion {...props} state={state} />}
+		</>
 	);
 }
