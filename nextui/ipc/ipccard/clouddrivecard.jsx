65480244f4a05bf20d34559b753657d9f3e71525
Jim Jin <jim_king_2000@163.com>
2024-02-27 11:10:51 +0800

    refactory

--- nextui/ipc/ipccard/clouddrivecard.jsx
+++ nextui/ipc/ipccard/clouddrivecard.jsx
@@ -19,7 +19,7 @@ import { ChevronRight } from '@styled-icons/material/ChevronRight';
 
 // The parameters 'occupied' and 'totle' designate for the occupied
 // and the totle space of the cloud drive. The units are MB.
-export function CloudDriveCard({ occupied, totle }) {
+export function CloudDriveCard({ src, occupied, totle }) {
 	const t = useJuJiuT();
 
 	return (
@@ -28,7 +28,7 @@ export function CloudDriveCard({ occupied, totle }) {
 				{t('正在为你长期保存文件')}
 			</CardHeader>
 			<CardBody className='flex flex-row gap-4'>
-				<Image src='https://ts1.cn.mm.bing.net/th/id/R-C.f54c83f04442cec528a250d251251ce6?rik=JE7BoZk5xK4iEg&riu=http%3a%2f%2fpic4.bbzhi.com%2ffengjingbizhi%2fgaoqingxifengjingzhuomianbizhixiazai%2fgaoqingxifengjingzhuomianbizhixiazai_366146_18.jpg&ehk=YvUnl11nBp%2fGJssQUbYqkXLo7fchkD%2fEQ8BGpW2Urjs%3d&risl=&pid=ImgRaw&r=0' />
+				<Image src={src} />
 				<div className='flex flex-col items-center gap-4 justify-center'>
 					<CircularProgress
 						size='lg'
