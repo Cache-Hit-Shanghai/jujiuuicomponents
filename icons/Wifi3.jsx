3dd568f923f477cf9749b9b683cacec2b8b55f5f
Jim Jin <jim_king_2000@163.com>
2024-08-19 17:26:17 +0800

    change color to 'currentColor'

--- icons/Wifi3.jsx
+++ icons/Wifi3.jsx
@@ -18,8 +18,8 @@ export const Wifi3 = ({
 			<path
 				id='Vector'
 				d='M2 9.48265C2.2944 9.20365 2.59761 8.9393 2.9087 8.6896C8.51855 4.18711 16.6911 4.45146 22 9.48265'
-				stroke='black'
-				stroke-opacity='0.2'
+				stroke='currentColor'
+				strokeOpacity='0.2'
 				strokeWidth='2'
 				strokeLinecap='round'
 				strokeLinejoin='round'
@@ -27,7 +27,7 @@ export const Wifi3 = ({
 			<path
 				id='Vector_2'
 				d='M19 12.8995C15.134 9.0335 8.866 9.0335 5 12.8995'
-				stroke='black'
+				stroke='currentColor'
 				strokeWidth='2'
 				strokeLinecap='round'
 				strokeLinejoin='round'
@@ -35,7 +35,7 @@ export const Wifi3 = ({
 			<path
 				id='Vector_3'
 				d='M16 16.1569C13.7908 13.9477 10.2092 13.9477 8 16.1569'
-				stroke='black'
+				stroke='currentColor'
 				strokeWidth='2'
 				strokeLinecap='round'
 				strokeLinejoin='round'
@@ -45,7 +45,7 @@ export const Wifi3 = ({
 				fillRule='evenodd'
 				clipRule='evenodd'
 				d='M12 20C12.6904 20 13.25 19.4404 13.25 18.75C13.25 18.0597 12.6904 17.5 12 17.5C11.3097 17.5 10.75 18.0597 10.75 18.75C10.75 19.4404 11.3097 20 12 20Z'
-				fill='black'
+				fill='currentColor'
 			/>
 		</g>
 	</svg>
