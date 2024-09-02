e96c71adb65a53d9aa5b45948a84773c8a19f77b
Jim Jin <jim_king_2000@163.com>
2024-09-02 16:37:35 +0800

    change notification

--- nextui/ipc/settings/notification.js
+++ nextui/ipc/settings/notification.js
@@ -5,18 +5,19 @@ import { Divider, Switch } from '@nextui-org/react';
 import { useJuJiuT } from '@/state/translate';
 
 const notiKeyList = [
-	{ title: '设备访问', key: 'deviceAccess' },
-	{ title: '设备状态', key: 'deviceState' },
-	{ title: '设备异常', key: 'deviceDeviant' },
-	{ title: '设备分享', key: 'friendsShare' },
+	// { title: '设备访问', key: 'deviceAccess' },
+	{ title: '设备提醒', key: 'deviceState' },
+	// { title: '设备异常', key: 'deviceDeviant' },
+	// { title: '设备分享', key: 'friendsShare' },
 	{ title: '服务提醒', key: 'serviceAlert' },
 	{ title: '公告', key: 'notice' },
 ];
 
 const notiTypeKeyList = [
-	{ key: 'framePeopleMove', title: '画面有人移动' },
-	{ key: 'frameFaceDetection', title: '画面人脸识别结果' },
-	{ key: 'nursePeopleMove', title: '看护区域有人移动' },
+	// { key: 'framePeopleMove', title: '画面有人移动' },
+	// { key: 'frameFaceDetection', title: '画面人脸识别结果' },
+	// { key: 'nursePeopleMove', title: '看护区域有人移动' },
+	{ key: 'aiRecognition', title: 'AI识别结果提醒' },
 ];
 
 export function ChangeNotification({
