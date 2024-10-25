95926b2220201876369b737cf984c581f7051bde
Yuhui <1573531544@qq.com>
2024-10-25 19:11:38 +0800

    reset change

--- nextui/ipc/video/video.jsx
+++ nextui/ipc/video/video.jsx
@@ -1,66 +1,61 @@
+import { useState } from 'react';
+import { useIdleTimer } from 'react-idle-timer';
+import { FadeAnimation, NavbarBack } from '../../core/core-ui';
 import { DocumentLandscapeStyle } from '@/components';
 import { ChevronLeft } from 'styled-icons/material';
 import { twMerge } from 'tailwind-merge';
 import { Button } from '@nextui-org/react';
-import { useRef } from 'react';
-import { documentFullscreen } from '@/util/document';
 
 export const VideoPlayerWrapper = ({
 	fullscreen,
-	setFullscreen,
+	fullscreenToggle,
 	fullscreenLabel = '退出全屏',
-	renderVideo,
+	videoPlayer,
 	wrapperClass,
 }) => {
-	const toggleScreenRef = useRef(null);
-	const onReady = (player) => {
-		const customFullscreenToggle = player
-			.getChild('ControlBar')
-			.addChild('button', {
-				clickHandler() {
-					const target = !document.fullscreenElement;
-					documentFullscreen(target);
-					setFullscreen(target);
-					customFullscreenToggle.setIcon(
-						target ? 'fullscreen-exit' : 'fullscreen-enter',
-					);
-				},
-			});
-		customFullscreenToggle.setIcon('fullscreen-enter');
-		toggleScreenRef.current = customFullscreenToggle;
-	};
+	const [showOverlayUtils, setShowOverlayUtils] = useState(true);
+	const [isMouseHover, setIsMouseHover] = useState(true);
 
-	const fullscreenToggle = () => {
-		toggleScreenRef.current?.handleClick?.();
-	};
+	useIdleTimer({
+		onIdle: () => setShowOverlayUtils(false),
+		onActive: () => setShowOverlayUtils(true),
+		timeout: 1000,
+		throttle: 500,
+	});
 
 	return (
 		<div
 			className={twMerge(
-				`flex flex-col justify-center bg-background w-full h-full  ${
-					fullscreen ? 'fixed top-0 left-0 z-[1111]' : 'relative'
+				`flex flex-col justify-center bg-background w-full h-full ${
+					fullscreen ? 'fixed top-0 left-0' : 'relative'
 				} `,
 				wrapperClass,
 			)}
+			onMouseLeave={() => {
+				fullscreen && isMouseHover && setIsMouseHover(false);
+			}}
+			onMouseEnter={() => {
+				fullscreen && !isMouseHover && setIsMouseHover(true);
+			}}
 		>
-			{fullscreen && (
-				<div
-					className={`absolute left-0 top-0 text-white z-[1010] w-full h-10`}
-				>
-					<Button
-						onClick={fullscreenToggle}
-						variant='light'
-						className='text-white'
-					>
-						<span className='font-semibold text-with-shadow '>
-							<ChevronLeft size={24} />
-						</span>
-						<span className='text-sm'>{fullscreenLabel}</span>
-					</Button>
-				</div>
+			{fullscreen && isMouseHover && (
+				<FadeAnimation show={showOverlayUtils}>
+					<div className='absolute left-0 top-0 text-white z-[10010]'>
+						<Button
+							onClick={fullscreenToggle}
+							variant='light'
+							className='text-white'
+						>
+							<span className='font-semibold text-with-shadow '>
+								<ChevronLeft size={24} />
+							</span>
+							<span className='text-sm'>{fullscreenLabel}</span>
+						</Button>
+					</div>
+				</FadeAnimation>
 			)}
 			<DocumentLandscapeStyle enabled={fullscreen} />
-			{renderVideo(onReady)}
+			{videoPlayer}
 		</div>
 	);
 };
