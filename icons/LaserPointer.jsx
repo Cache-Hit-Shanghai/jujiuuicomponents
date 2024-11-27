71913cdd234d051a0a56f5dca50fb93738207ef4
Yuhui <1573531544@qq.com>
2024-11-27 17:27:14 +0800

    Add icon

---
+++ icons/LaserPointer.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+const LaserPointer = (props) => (
+	<svg
+		id='\u56FE\u5C42_1'
+		data-name='\u56FE\u5C42 1'
+		xmlns='http://www.w3.org/2000/svg'
+		viewBox='0 0 24 24'
+		width={24}
+		height={24}
+		{...props}
+	>
+		<defs>
+			<style>
+				{
+					'.cls-1{fill:#fff;}.cls-2{fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px;stroke-dasharray:0.87244 4.36218 0.87244 4.36218 0.87244 4.36218;}'
+				}
+			</style>
+		</defs>
+		<circle className='cls-1' cx={12} cy={12.28346} r={4.82619} />
+		<circle className='cls-2' cx={12} cy={12.28346} r={10} />
+	</svg>
+);
+export default LaserPointer;
