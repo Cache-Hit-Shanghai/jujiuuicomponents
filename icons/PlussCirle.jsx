337013e8ac0fde41bb9907a151c9b5d9ee321457
xiaoke_1256 <xiaoke_1256@sina.com>
2024-11-28 19:15:32 +0800

    add camera and pluss icon

---
+++ icons/PlussCirle.jsx
@@ -0,0 +1,17 @@
+export const PlussCirle = ({fill = '#4c4c4c',...props}) => (
+    <svg
+        aria-hidden='true'
+        focusable='false'
+        height='24'
+        role='presentation'
+        viewBox='0 0 24 24'
+        width='24'
+        fill={fill}
+        {...props}
+    >
+        <defs><style>{".cls-1{fill:#4c4c4c;}.cls-2{fill:#fff;}"}</style></defs>
+        <circle class="cls-1" cx="12.05224" cy="12" r="11.00738"/>
+        <rect class="cls-2" x="5.98617" y="11.06608" width="11.92504" height="2.10108"/>
+        <rect class="cls-2" x="5.98617" y="11.06608" width="11.92504" height="2.10108" transform="translate(-0.16793 24.0653) rotate(-90)"/>
+    </svg>
+);
