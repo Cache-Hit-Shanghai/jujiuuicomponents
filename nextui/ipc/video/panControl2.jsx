432c8bcd1fef35ec674a2ec5482629c253ca4bce
Yuhui <1573531544@qq.com>
2024-12-13 09:39:13 +0800

    ReactNipple change

--- nextui/ipc/video/panControl2.jsx
+++ nextui/ipc/video/panControl2.jsx
@@ -170,7 +170,7 @@ export function PanControl3({
 		} else {
 			degree = (450 - data.angle.degree) % 360;
 		}
-		const distance = Number((data?.distance / 80).toFixed(3));
+		const distance = Number((data?.distance / 60).toFixed(3));
 		console.log('distance:>>', distance, degree);
 		degree = Number(degree.toFixed(1));
 		if (
