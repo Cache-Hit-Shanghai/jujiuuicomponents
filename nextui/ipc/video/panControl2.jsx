3ed2a7e12d422427c671c746088e2ec851de50ab
xiaoke_1256 <xiaoke_1256@sina.com>
2025-01-08 16:49:28 +0800

    add speed controller for PanControlL1Version1

--- nextui/ipc/video/panControl2.jsx
+++ nextui/ipc/video/panControl2.jsx
@@ -241,6 +241,7 @@ export function PanControlL1Version1 ({
 	isDisabled,
 	needReload,
 	isSelected,
+	speedNum,
 }) {
 	const mastStyle =
 		'radial-gradient(circle farthest-side at bottom right, transparent 40%, #000 40%)';
@@ -252,7 +253,7 @@ export function PanControlL1Version1 ({
 		}, 100);
 	}, [needReload, isSelected]);
 
-	let previousDistance = null;
+	//let previousDistance = null;
 	let previousDegree = null;
 
 	const onMoveHandler = (event, data) => {
@@ -262,16 +263,17 @@ export function PanControlL1Version1 ({
 		} else {
 			degree = (450 - data.angle.degree) % 360;
 		}
-		const distance = Number((data?.distance / 70).toFixed(3));
+		//const distance = Number((data?.distance / 70).toFixed(3));
 		degree = Number(degree.toFixed(1));
 		if (
-			previousDistance === null ||
+			//previousDistance === null ||
 			previousDegree === null ||
-			Math.abs(previousDegree - degree) >= 5 ||
-			Math.abs(previousDistance - distance) >= 0.1
+			Math.abs(previousDegree - degree) >= 5 
+			//|| Math.abs(previousDistance - distance) >= 0.1
 		) {
-			onMove?.(distance, degree, 1);
-			previousDistance = distance;
+			console.log("(speedNum/100):",Number((speedNum/100).toFixed(3)));
+			onMove?.(Number((speedNum/100).toFixed(3)), degree, 1);
+			//previousDistance = distance;
 			previousDegree = degree;
 		}
 	};
@@ -282,7 +284,7 @@ export function PanControlL1Version1 ({
 	);
 
 	const onEndHandler = () => {
-		previousDistance = null;
+		//previousDistance = null;
 		previousDegree = null;
 		handleReset();
 	};
